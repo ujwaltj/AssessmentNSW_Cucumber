7726e490e31d9f0886283e0786d3923127d29444
ujwaltj <ujwal.tj7@gmail.com>
2024-08-11 18:05:50 +0530

    Revert "Initial Veriosn of Cucumber Framework for NSW Assessment task"
    This reverts commit b8d65fcb5aad53d9901972329ac9e3dfbf99bca7.

--- src/main/java/org/nsw/gov/framework/WebdriverInitializer.java
+++ src/main/java/org/nsw/gov/framework/WebdriverInitializer.java
@@ -42,7 +42,7 @@ public class WebdriverInitializer {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
-
+                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
 
